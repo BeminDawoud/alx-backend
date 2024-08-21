9f345de60808a280cec61a4eaa6afc0837ba968b
Bemin Dawoud <bemin_dawoud@yahoo.com>
2024-08-21 21:56:21 +0300

    Create task 0

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" BaseCache module
+"""
+from basic_cashing import BaseCaching
+
+
+class BaseCache(BaseCaching):
+    """BaseCache Completes BaseCaching"""
+
+    def put(self, key, item):
+        """Add an item in the cache"""
+        if key and item:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """Get an item by key"""
+        if key and self.cache_data[key]:
+            return self.cache_data[key]
+        return None
