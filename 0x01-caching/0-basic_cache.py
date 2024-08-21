8bcf288637b95bbd826a0c33adbdf760b4488d84
Bemin Dawoud <bemin_dawoud@yahoo.com>
2024-08-21 22:03:54 +0300

    enhance get

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -5,15 +5,14 @@ from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """BaseCache Completes BaseCaching"""
+    """BaseCache Completes BaseCaching and retrieves items and adds them"""
 
     def put(self, key, item):
         """Add an item in the cache"""
         if key and item:
             self.cache_data[key] = item
+        return
 
     def get(self, key):
         """Get an item by key"""
-        if key and self.cache_data[key]:
-            return self.cache_data[key]
-        return None
+        return self.cache_data.get(key, None)
