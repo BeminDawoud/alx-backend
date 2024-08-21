f6dcd4ea472ea9ba08573ef822bc222d92c7bd4a
Bemin Dawoud <bemin_dawoud@yahoo.com>
2024-08-21 04:41:49 +0300

    Add documentation

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -6,6 +6,9 @@ from typing import Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    task 0
+    """
     start = page_size * (page - 1)
     end = start + page_size
     return (start, end)
