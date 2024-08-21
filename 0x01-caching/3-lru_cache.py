227d0f0e42f2551ae564b68068af552d0ba19705
Bemin Dawoud <bemin_dawoud@yahoo.com>
2024-08-21 22:47:49 +0300

    Improve task 3

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,22 +1,37 @@
-#!/usr/bin/python3
-""" BaseCache module
+#!/usr/bin/env python3
+"""Least Recently Used caching module.
 """
+from collections import OrderedDict
+
 from base_caching import BaseCaching
 
 
 class LRUCache(BaseCaching):
-    """BaseCache Completes BaseCaching and retrieves items and adds them"""
+    """Represents an object that allows storing and
+    retrieving items from a dictionary with a LRU
+    removal mechanism when the limit is reached.
+    """
+
+    def __init__(self):
+        """Initializes the cache."""
+        super().__init__()
+        self.cache_data = OrderedDict()
 
     def put(self, key, item):
-        """Add an item in the cache"""
-        if key and item:
-            if len(self.cache_data) == BaseCaching.MAX_ITEMS:
-                last_key, _ = self.cache_data.popitem()
-                print(f"DISCARD: {last_key}")
+        """Adds an item in the cache."""
+        if key is None or item is None:
+            return
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                lru_key, _ = self.cache_data.popitem(True)
+                print("DISCARD:", lru_key)
+            self.cache_data[key] = item
+            self.cache_data.move_to_end(key, last=False)
+        else:
             self.cache_data[key] = item
 
-        return
-
     def get(self, key):
-        """Get an item by key"""
+        """Retrieves an item by key."""
+        if key is not None and key in self.cache_data:
+            self.cache_data.move_to_end(key, last=False)
         return self.cache_data.get(key, None)
